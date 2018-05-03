728ec87c59aeda71793d770abdd0e441a1b36e6e
mxmCherry <mxmCherry@users.noreply.github.com>
2018-05-03 11:34:16 +0300

    Godoc comments

--- limitgroup.go
+++ limitgroup.go
@@ -1,3 +1,4 @@
+// Package limitgroup provides a control structure for a group of goroutines with configurable concurrency (max number of goroutines to run in parallel).
 package limitgroup
 
 import (
@@ -6,14 +7,14 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-// Group is a limiting errgroup
+// Group is a concurrency-limiting errgroup.
 type Group struct {
 	limiter chan struct{}
 	parent  *errgroup.Group
 	ctx     context.Context
 }
 
-// New inits a new group with a given concurrency (default: 1)
+// New inits a new group with a given concurrency (default: 1).
 func New(ctx context.Context, concurrency int) (*Group, context.Context) {
 	if concurrency < 1 {
 		concurrency = 1
@@ -27,7 +28,8 @@ func New(ctx context.Context, concurrency int) (*Group, context.Context) {
 	}, ctx
 }
 
-// Go runs a function as part of the group. The first function to
+// Go runs a function as part of the group.
+// If group's context is cancelled, this is a no-op.
 func (g *Group) Go(fn func() error) {
 	select {
 	case <-g.ctx.Done():
